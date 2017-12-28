94d1505ae985195afaadcc17c78c3779ce6c0c0f
Nils Homer <nilshomer@gmail.com>
2017-12-28 00:13:17 -0700

    Adding sample tests

--- src/basespace_commons/sample.py
+++ src/basespace_commons/sample.py
@@ -15,8 +15,8 @@ class Sample:
         """
         self.__dict = sample_dict
         self.__sample_ordinal = sample_ordinal
-        assert 'sample_name' in sample_dict
-        assert 'sample_id' in sample_dict
+        assert sample_dict['sample_name']
+        assert sample_dict['sample_id']
         assert 0 < self.__sample_ordinal
 
     def bam(self, dir, ext="bam", unmatched=False, illumina_naming=False, sample_barcode_column=None):
@@ -27,8 +27,6 @@ class Sample:
         """
         prefix = self.prefix(unmatched=unmatched, illumina_naming=illumina_naming, sample_barcode_column=sample_barcode_column)
         return os.path.join(dir, f"{prefix}.{ext}")
-
-        return os.path.join(dir, f"{sample_name}_S{self.__sample_ordinal:d}_L001.{ext}")
 
     def fq(self, dir, end, unmatched=None,illumina_naming=False, sample_barcode_column=None):
         """
@@ -42,7 +40,7 @@ class Sample:
         assert 1 == end or 2 == end 
         ext = Environment.fq_ext(end=end)
         prefix = self.prefix(unmatched=unmatched, illumina_naming=illumina_naming, sample_barcode_column=sample_barcode_column)
-        return os.path.join(dir, f"{prefix}.{ext}")
+        return os.path.join(dir, f"{prefix}_{ext}")
 
     def prefix(self, unmatched=False, illumina_naming=False, sample_barcode_column=None):
         """
@@ -60,14 +58,23 @@ class Sample:
         if illumina_naming:
             return f"{sample_name}_S{self.__sample_ordinal:d}_L001"
         else:
+            assert sample_barcode_column
             sample_barcode_column = sample_barcode_column.lower()
-            sample_id              = self.__dict['sample_id']
+            sample_id             = self.__dict['sample_id']
             sample_barcode_bases  = self.__dict[sample_barcode_column]
             return f"{sample_id}-{sample_name}-{sample_barcode_bases}"
+    
+    def sample_ordinal(self):
+        """ Gets the sample ordinal. """
+        return self.__sample_ordinal
 
     def sample_name(self):
         """ Gets the sample name. """
         return self.__dict['sample_name']
+
+    def sample_id(self):
+        """ Gets the sample id. """
+        return self.__dict['sample_id']
 
     def library_id(self):
         """ Gets the library id, or sample id if library id is missing. """
@@ -86,7 +93,8 @@ class Sample:
         Reads an Illumina Experiment Manager Sample Sheet or metadata input file and returns
         a list of samples.
         """
-        lines = [line.rstrip("\r\n") for line in data.split("\r\n")]
+        lines = [line.strip().rstrip("\r\n") for line in data.splitlines()]
+        lines = [line for line in lines if len(line) > 0]
         line_iter = iter(line for line in lines)
         try:
             next(iter(line for line in lines if "[Data]" in line))
@@ -103,6 +111,8 @@ class Sample:
         samples = []
         header = [line.lower() for line in next(line_iter).split(",")]
         for sample_index, line in enumerate(line_iter):
+            if len(line) == 0:
+                break
             sample_data = line.split(",")
             sample_dict = dict(zip(header, sample_data))
             sample      = Sample(sample_dict, sample_ordinal=sample_index+1)
--- src/basespace_commons/tests/test_sample.py
+++ src/basespace_commons/tests/test_sample.py
@@ -1,35 +1,128 @@
 import unittest
 
-from basespace_commons import sample
+from basespace_commons.sample import Sample
 
 class TestSample(unittest.TestCase):
-    # Tests to write
 
-    # init a sample, require 'sample_name'
-    # init a sample, require 'sample_id'
-    # init a sample, sample ordinal > 0
+    def test_init_require_sample_name(self):
+        with self.assertRaises(KeyError):
+            Sample({'sample_id' : 'id'}, 1)
+        with self.assertRaises(AssertionError):
+            sample = Sample({'sample_name' : None, 'sample_id' : 'id'}, 1)
 
-    # bam() with a matched/unmatched sample
-    # bam() with illumina_naming True/False
-    # bam() with illumina_naming False but no sample_barcode_column
+    def test_init_require_sample_id(self):
+        with self.assertRaises(KeyError):
+            Sample({'sample_name' : 'name'}, 1)
+        with self.assertRaises(AssertionError):
+            sample = Sample({'sample_name' : 'name', 'sample_id' : None}, 1)
+
+    def test_init_require_sample_ordinal_geq_one(self):
+        with self.assertRaises(AssertionError):
+            Sample({'sample_name' : 'name', 'sample_id' : 'id'}, 0)
+
+    def test_init_ok(self):
+        sample = Sample({'sample_name' : 'name', 'sample_id' : 'id'}, 1)
+        self.assertEqual(sample.sample_name(), 'name')
+        self.assertEqual(sample.sample_id(), 'id')
+        self.assertEqual(sample.sample_ordinal(), 1)
+
+    def test_bam_matched(self):
+        matchedSample = Sample({'sample_name' : 'matched', 'sample_id' : 'id', 'sample_barcode' : 'GATTACA'}, 1) 
+        self.assertEqual(matchedSample.bam(dir='dir', unmatched=False, illumina_naming=False, sample_barcode_column='sample_barcode'), 'dir/id-matched-GATTACA.bam')
+        self.assertEqual(matchedSample.bam(dir='dir', unmatched=False, illumina_naming=True, sample_barcode_column=None), 'dir/matched_S1_L001.bam')
+
+    def test_bam_unmatched(self):
+        unmatchedSample = Sample({'sample_name' : 'unmatched', 'sample_id' : 'id', 'sample_barcode' : 'NNNNNNN'}, 2) 
+        self.assertEqual(unmatchedSample.bam(dir='dir', unmatched=True, illumina_naming=False, sample_barcode_column='sample_barcode'), 'dir/id-unmatched-NNNNNNN.bam')
+        self.assertEqual(unmatchedSample.bam(dir='dir', unmatched=True, illumina_naming=True, sample_barcode_column='sample_barcode'), 'dir/unmatched_S2_L001.bam')
+
+    def test_fq_matched(self):
+        matchedSample = Sample({'sample_name' : 'matched', 'sample_id' : 'id', 'sample_barcode' : 'GATTACA'}, 1) 
+        self.assertEqual(matchedSample.fq(dir='dir', end=1, unmatched=False, illumina_naming=False, sample_barcode_column='sample_barcode'), 'dir/id-matched-GATTACA_R1_001.fastq.gz')
+        self.assertEqual(matchedSample.fq(dir='dir', end=1, unmatched=False, illumina_naming=True, sample_barcode_column=None), 'dir/matched_S1_L001_R1_001.fastq.gz')
+        self.assertEqual(matchedSample.fq(dir='dir', end=2, unmatched=False, illumina_naming=False, sample_barcode_column='sample_barcode'), 'dir/id-matched-GATTACA_R2_001.fastq.gz')
+        self.assertEqual(matchedSample.fq(dir='dir', end=2, unmatched=False, illumina_naming=True, sample_barcode_column=None), 'dir/matched_S1_L001_R2_001.fastq.gz')
+
+    def test_fq_unmatched(self):
+        unmatchedSample = Sample({'sample_name' : 'unmatched', 'sample_id' : 'id', 'sample_barcode' : 'NNNNNNN'}, 2) 
+        self.assertEqual(unmatchedSample.fq(dir='dir', end=1, unmatched=True, illumina_naming=False, sample_barcode_column='sample_barcode'), 'dir/id-unmatched-NNNNNNN_R1_001.fastq.gz')
+        self.assertEqual(unmatchedSample.fq(dir='dir', end=1, unmatched=True, illumina_naming=True, sample_barcode_column='sample_barcode'), 'dir/unmatched_S2_L001_R1_001.fastq.gz')
+        self.assertEqual(unmatchedSample.fq(dir='dir', end=2, unmatched=True, illumina_naming=False, sample_barcode_column='sample_barcode'), 'dir/id-unmatched-NNNNNNN_R2_001.fastq.gz')
+        self.assertEqual(unmatchedSample.fq(dir='dir', end=2, unmatched=True, illumina_naming=True, sample_barcode_column='sample_barcode'), 'dir/unmatched_S2_L001_R2_001.fastq.gz')
     
-    # fq() with end 1 or 2
-    # fq() with end out of range
-    # fq() with a matched/unmatched sample
-    # fq() with illumina_naming True/False
-    # fq() with illumina_naming False but no sample_barcode_column
+    def test_prefix_matched(self):
+        matchedSample = Sample({'sample_name' : 'matched', 'sample_id' : 'id', 'sample_barcode' : 'GATTACA'}, 1) 
+        self.assertEqual(matchedSample.prefix(unmatched=False, illumina_naming=False, sample_barcode_column='sample_barcode'), 'id-matched-GATTACA')
+        self.assertEqual(matchedSample.prefix(unmatched=False, illumina_naming=True, sample_barcode_column=None), 'matched_S1_L001')
+        with self.assertRaises(AssertionError):
+            matchedSample.prefix(unmatched=False, illumina_naming=False, sample_barcode_column=None)
+
+    def test_prefix_unmatched(self):
+        unmatchedSample = Sample({'sample_name' : 'unmatched', 'sample_id' : 'id', 'sample_barcode' : 'NNNNNNN'}, 2) 
+        self.assertEqual(unmatchedSample.prefix(unmatched=True, illumina_naming=False, sample_barcode_column='sample_barcode'), 'id-unmatched-NNNNNNN')
+        self.assertEqual(unmatchedSample.prefix(unmatched=True, illumina_naming=True, sample_barcode_column='sample_barcode'), 'unmatched_S2_L001')
+        with self.assertRaises(AssertionError):
+            unmatchedSample.prefix(unmatched=True, illumina_naming=False, sample_barcode_column=None)
     
-    # prefix() with a matched/unmatched sample
-    # prefix() with illumina_naming True/False
-    # prefix() with illumina_naming False but no sample_barcode_column
+    def test_library_id(self):
+        libraryId = Sample({'sample_name' : 'matched', 'sample_id' : 'id', 'sample_barcode' : 'GATTACA', 'library_id' : 'lid'}, 1) 
+        self.assertEqual(libraryId.library_id(), 'lid')
 
-    # get_sample_name()
+        noLibraryId = Sample({'sample_name' : 'matched', 'sample_id' : 'id', 'sample_barcode' : 'GATTACA'}, 1) 
+        self.assertEqual(noLibraryId.library_id(), 'id')
 
-    # library_id with library id present/missing
+    def test_get(self):
+        sample = Sample({'sample_name' : 'matched', 'sample_id' : 'id', 'sample_barcode' : 'GATTACA', 'fun_key' : 'fun_value'}, 1) 
+        self.assertEqual(sample.get('fun_key'), 'fun_value')
+        with self.assertRaises(KeyError):
+            self.assertEqual(sample.get('boring_key'))
 
-    # get arbitrary key 
+    def test_samples_from_sample_sheet(self):
+        sample_sheet = """
+[Header],,,,,,,,,,,
+IEMFileVersion,4,,,,,,,,,,
+Investigator Name,Joe,,,,,,,,,,
+Experiment Name,EXPID,,,,,,,,,,
+Date,1/1/00,,,,,,,,,,
+Workflow,GenerateFASTQ,,,,,,,,,,
+Application,FASTQ Only,,,,,,,,,,
+Assay,Assay Name,,,,,,,,,,
+Description,The Description,,,,,,,,,,
+Chemistry,Amplicon,,,,,,,,,,
+,,,,,,,,,,,
+[Reads],,,,,,,,,,,
+151,,,,,,,,,,,
+151,,,,,,,,,,,
+,,,,,,,,,,,
+[Settings],,,,,,,,,,,
+ReverseComplement,0,,,,,,,,,,
+Adapter,AGATCGGAAGAGCACACGTCTGAACTCCAGTCA,,,,,,,,,,
+AdapterRead2,AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT,,,,,,,,,,
+,,,,,,,,,,,
+[Data],,,,,,,,,,,
+Sample_ID,Sample_Name,Sample_Plate,Sample_Well,Sample_Barcode,R2_Barcode_Bases,I7_Index_ID,index,I5_Index_ID,index2,Sample_Project,Description
+1,N1,,,AAAAAAAA,,,,,,,
+2,N2,,,CCCCCCCC,,,,,,,
+3,N3,,,GGGGGGGG,,,,,,,
+4,N4,,,TTTTTTTT,,,,,,,
+5,N5,,,NNNNNNNN,,,,,,,
+        """
+        samples = Sample.samples_from(data=sample_sheet, logging=False)
+        self.assertListEqual([s.sample_id() for s in samples], ["1", "2", "3", "4", "5"])
+        self.assertListEqual([s.sample_name() for s in samples], ["N1", "N2", "N3", "N4", "N5"])
+        self.assertListEqual([s.get('sample_barcode') for s in samples], ["A"*8, "C"*8, "G"*8, "T"*8, "N"*8])
 
-    # samples_from() SampleSheet.csv
-    # samples_from() metadata.csv
+    def test_samples_from_metadata(self):
+        sample_sheet = """
+Sample_ID,Sample_Name,Sample_Plate,Sample_Well,Sample_Barcode,R2_Barcode_Bases,I7_Index_ID,index,I5_Index_ID,index2,Sample_Project,Description
+1,N1,,,AAAAAAAA,,,,,,,
+2,N2,,,CCCCCCCC,,,,,,,
+3,N3,,,GGGGGGGG,,,,,,,
+4,N4,,,TTTTTTTT,,,,,,,
+5,N5,,,NNNNNNNN,,,,,,,
+        """
+        samples = Sample.samples_from(data=sample_sheet, logging=False)
+        self.assertListEqual([s.sample_id() for s in samples], ["1", "2", "3", "4", "5"])
+        self.assertListEqual([s.sample_name() for s in samples], ["N1", "N2", "N3", "N4", "N5"])
+        self.assertListEqual([s.get('sample_barcode') for s in samples], ["A"*8, "C"*8, "G"*8, "T"*8, "N"*8])
 
-    pass
